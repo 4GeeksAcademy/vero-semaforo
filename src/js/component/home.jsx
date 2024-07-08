cb3a33447c621c9fc21ceb4b07be1a4ca4e6dfcf
Vero519 <voviedo6@gmail.com>
2024-07-08 15:13:42 +0000

    semaforo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,47 @@
-import React from "react";
 
+import React, { useState } from "react"
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+//import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [luces, setLuces] = useState("secondary")
+	const [lucesAmarillas, setLucesAmarillas] = useState("secondary")
+	const [lucesVerdes, setLucesVerdes] = useState("secondary")
+
+	function cambiarColor() {
+			setLuces("danger")
+			setLucesAmarillas("secondary")
+			setLucesVerdes ("secondary")
+
+
+	}
+	function cambiarLucesAmarillas () {
+		setLucesAmarillas ("warning")
+		setLuces ("secondary")
+		setLucesVerdes ("secondary")
+	}
+	function cambiarLucesVerdes() {
+		setLucesVerdes ("success")
+		setLuces ("secondary")
+		setLucesAmarillas ("secondary")
+		
+	}
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container border border-1" style={{width:"120px", marginTop:"20px", padding:"10px", background:"black"}}>
+
+			<div className={`alert alert-${luces} rounded-circle`} onClick={cambiarColor}>
+			</div>
+			<div className={`alert alert-${lucesAmarillas} rounded-circle`}  onClick={cambiarLucesAmarillas}>
+			</div>
+			<div className={`alert alert-${lucesVerdes} rounded-circle`} onClick={cambiarLucesVerdes}> 
+			</div>
+
+
+
 		</div>
+
+
 	);
 };
 
